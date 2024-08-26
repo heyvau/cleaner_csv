e6199dfc500af55f22b09d9981b2c3d5317642ba
unknown <=>
2024-08-26 16:02:51 +0200

    update

--- cleaner_csv.py
+++ cleaner_csv.py
@@ -1,67 +1,187 @@
 import pandas as pd
-from utilities import FileReader
+from utilities import FileHandler, keys_checking
+from ydata_profiling import ProfileReport
+import logging
+
+
+logging.basicConfig(
+    filename="cleaner.log",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s (%(filename)s)\n%(name)s-%(levelname)s: %(message)s"
+)
+
 
 class CleanerCSV:
-    def __init__(self, specsfile: str = "default_specs.json"):
-        self.specs = FileReader.read_json(filename=specsfile)
-        self.df_original = FileReader.read_csv(
+    def __init__(self, specsfile: str = "default_specs.json") -> None:
+        """
+        Initialization method gets cleaning parameters
+        and instructions from JSON specification file,
+
+        original data set from specified CSV file and saves
+        its original version and a deep copy as pandas data frames.
+
+        """
+        self.specs = FileHandler.read_json(filename=specsfile)
+
+        self.df_original = FileHandler.read_csv(
             filename=self.specs.get("input_file"),
             sep=self.specs.get("delimiter")
         )
         self.df_copy = self.df_original.copy()
 
     
-    def clean(self):
+    def clean(self) -> None:
+        """
+        Main method for managing data cleaning methods
+        based on specification.
+    """
         if self.specs.get("drop_duplicates"):
             self._drop_duplicates()
+            logging.info("Drop duplicates.")
 
         if self.specs.get("drop_na"):
             self._drop_na()
+            logging.info("Drop NaN values-containing rows.")
 
-        if self.specs.get("clean_str_columns"):
-            self._clean_str_columns()
+        if drop_col := self.specs.get("drop_col"):
+            self._drop_columns(cols=drop_col)
+            logging.info("Drop specified columns.")
+
+        if self.specs.get("clean_str_columns") and (
+            str_col := self.specs.get("str_col")):
+            self._clean_str_columns(cols=str_col)
+            logging.info("Clean string columns.")
 
         if self.specs.get("drop_row_title"):
             self._drop_row_title()
+            logging.info("Drop rows identical to header.")
 
         if self.specs.get("replace_row_char"):
             self._replace_row_char()
+            logging.info("Replace chars in specified columns.")
+
+        self._correct_data_type()
+        logging.info("Data type correction.")
 
         if self.specs.get("clean_outliers"):
             self._clean_outliers()
 
         if self.specs.get("export_output_file"):
             self._export_output_file()
-
-    
-    def _drop_duplicates(self):
-        pass
+            logging.info("Export to CSV.")
 
 
-    def _drop_na(self):
-        pass
-
-    
-    def _clean_str_columns(self):
-        pass
-
-
-    def _drop_row_title(self):
-        pass
+    @keys_checking
+    def _drop_duplicates(self) -> None:
+        """
+        Method removes duplicate rows.
+        """
+        self.df_copy.drop_duplicates(inplace=True)
 
 
-    def _replace_row_char(self):
-        pass
+    @keys_checking
+    def _drop_na(self) -> None:
+        """
+        Method removes NaN values-containing rows.
+        """
+        self.df_copy.dropna(how="all", inplace=True)
 
 
+    @keys_checking
+    def _drop_columns(self, cols: list[str]) -> None:
+        """
+        Method removes specified columns.
+        """
+        self.df_copy.drop(columns=cols, axis=1, inplace=True)
+
+
+    @keys_checking
+    def _clean_str_columns(self, cols: list[str]) -> None:
+        """
+        Method removes whitespaces in specified string columns.
+        """
+        self.df_copy[cols] = self.df_copy[cols].map(str.strip)
+
+
+    @keys_checking
+    def _drop_row_title(self) -> None:
+        """
+        Method removes rows identical to header row.
+        """
+        criteria = self.df_copy[self.df_copy.columns].ne(self.df_copy.columns).all(axis=1)
+        self.df_copy = self.df_copy[criteria]
+
+
+    @keys_checking
+    def _replace_row_char(self) -> None:
+        """
+        Method for replacing characters in specified columns.
+        """
+        replace_details = self.specs.get("replace_row_char_details")
+        cols = replace_details.get("col")
+        change = replace_details.get("change")
+
+        self.df_copy[cols] = self.df_copy[cols].replace(change, regex=True)
+
+
+    @keys_checking
+    def _correct_data_type(self) -> None:
+        """
+        Method for data type correction for specified columns.
+        """
+        numeric_cols = self.specs.get("numeric_col")
+        self.df_copy[numeric_cols] = self.df_copy[numeric_cols].map(pd.to_numeric)
+
+        datetime_cols = self.specs.get("datetime_col")
+        self.df_copy[datetime_cols] = self.df_copy[datetime_cols].map(pd.to_datetime)
+
+        convert_dict = {}
+        convert_dict.update({col: str for col in self.specs.get("str_col")})
+        convert_dict.update({col: float for col in self.specs.get("float_col")})
+        convert_dict.update({col: int for col in self.specs.get("int_col")})
+
+        self.df_copy = self.df_copy.astype(convert_dict)
+
+
+    @keys_checking
     def _clean_outliers(self):
         pass
 
 
-    def _export_output_file(self):
-        pass
+    def _export_output_file(self) -> None:
+        """
+        Method for saving cleaned data set to CSV file.
+        """
+        FileHandler.write_csv(
+            data=self.df_copy,
+            filename=self.specs.get("output_file"),
+            sep=self.specs.get("delimiter"),
+        )
 
 
-    
+    def create_profiles(self) -> None:
+        """
+        Method generates detailed reports for both the original and cleaned data
+        and summary report for comparing them.
+        """
+        original_file = self.specs.get("input_file_profile")
+        original_report = ProfileReport(self.df_original, title="Original")
+        FileHandler.create_profile(report=original_report, filename=original_file)
+
+        clean_file = self.specs.get("output_file_profile")
+        clean_report = ProfileReport(self.df_copy, title="Clean")
+        FileHandler.create_profile(report=clean_report, filename=clean_file)
+
+        try:
+            summary_file = self.specs.get("summary_file")
+            summary_report = original_report.compare(clean_report)
+            FileHandler.create_profile(report=summary_report, filename=summary_file)
+        except ValueError:
+            logging.error("After data type correction, comparison of data sets is not possible.")
+
+
 if __name__ == "__main__":
     c = CleanerCSV()
+    c.clean()
+    c.create_profiles()
--- utilities.py
+++ utilities.py
@@ -1,35 +1,98 @@
 import json
-import csv
+from ydata_profiling import ProfileReport
 import pandas as pd
 from pathlib import Path
+import logging
+import sys
 
 
-def abs_path(func):
+logger = logging.getLogger(__name__)
+
+
+def keys_checking(func):
+    """
+    Decorator for handling dictionary keys errors.
+    """
     def wrapper(*args, **kwargs):
-        filename = kwargs.get("filename")
-        kwargs["filename"] = Path(__file__).resolve().parent / filename
         try:
             return func(*args, **kwargs)
-        except FileNotFoundError:
-            print(f"Die Datei '{filename}' konnte nicht gefunden werden.")
+        except KeyError as error:
+            logger.error(f"Error in specification file. {error}")
     return wrapper
 
 
-class FileReader:
+def abs_path(func):
+    """
+    Decorator takes a file name and replaces it
+    with an absolute path to this file.
+    """
+    def wrapper(*args, **kwargs):
+        filename = kwargs.get("filename")
+        kwargs["filename"] = Path(__file__).resolve().parent / filename
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def check_path(func):
+    """
+    Decorator for handling errors in case
+    of non-existent files and directories.
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError as error:
+            logger.error(error)
+            sys.exit(1)
+        except OSError as error:
+            logger.error(error)
+    return wrapper
+
+
+
+class FileHandler:
+    """
+    The class contains file handling methods.
+    """
+
     @staticmethod
+    @check_path
     @abs_path
     def read_csv(filename: str, sep: str = ",") -> pd.DataFrame:
         """
-        A function returns data as pandas dataframe from a given CSV file.
+        Method returns data as pandas data frame from a given CSV file.
         """
         return pd.read_csv(filename, sep=sep)
 
 
     @staticmethod
+    @check_path
     @abs_path
     def read_json(filename: str) -> dict:
         """
-        A function returns data as dict from a given JSON file.
+        Method returns data as dict from a given JSON file.
         """
         with open(filename, mode="r", encoding="utf-8") as f:
                 return json.load(f)
+
+
+    @staticmethod
+    @check_path
+    @abs_path
+    def write_csv(data: pd.DataFrame, filename: str, sep: str = ",", index: bool = False) -> None:
+        """
+        Method saves pandas data frame to a given CSV file.
+        """
+        data.to_csv(filename, sep=sep, index=index)
+        logger.info(f"Data set has been successfully saved to {filename}.")
+
+
+    @staticmethod
+    @check_path
+    @abs_path
+    def create_profile(report: ProfileReport, filename: str) -> None:
+        """
+        Method creates html profile from a given profile report.
+        """
+        report.to_file(filename)
+        logger.info(f"Report file'{filename}' created.")
